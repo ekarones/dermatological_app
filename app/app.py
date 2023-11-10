8b831f4c76b1d00ee99010f4831d9b5fb0749f88
ekarones <erick19arones@gmail.com>
2023-11-10 15:46:45 -0500

    first commit

---
+++ app/app.py
@@ -0,0 +1,178 @@
+from flask import Flask, request, render_template, redirect, url_for, abort
+import sqlite3
+import os
+import base64
+import joblib
+import numpy as np
+import cv2
+base_dir = os.path.abspath(os.path.dirname(__file__))
+database_path = os.path.join(base_dir, 'database', 'my_database.db')
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+model_filename = 'svm_model.joblib'
+svm_model = joblib.load(model_filename)
+
+disease_folders = ["PSORIASIS", "ROSACEA", "SARPULLIDO", "VITILIGIO"]
+
+PACIENTE_ID = 1
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/add_patient', methods=['POST','GET'])
+def add_patient():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        documento = request.form['documento']
+        telefono = request.form['telefono']
+        date = request.form['fecha_nacimiento']
+        correo = request.form['correo']
+        contrasena = request.form['contrasena']
+        conn = sqlite3.connect(database_path)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO Patients (name, lastName,document,telf,date,email,password) VALUES (?, ?, ?, ?, ?, ?, ?)", (nombre, apellido,documento,telefono,date,correo,contrasena ))
+        conn.commit()
+        conn.close()
+        return redirect('/show_patients')
+    return render_template('add_patient.html')
+
+@app.route('/add_doctor',methods=['POST','GET'])
+def add_doctor():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        descripcion = request.form['descripcion']
+        conn = sqlite3.connect(database_path)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO Doctors (name, lastName, description) VALUES ( ?, ?, ?)", (nombre, apellido,descripcion ))
+        conn.commit()
+        conn.close()
+        return redirect('/show_doctors')
+    return render_template('add_doctor.html')
+
+@app.route('/show_patients')
+def show_patients():
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Patients")
+    resultados = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('show_patients.html',pacientes=resultados)
+
+@app.route('/show_doctors')
+def show_doctors():
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Doctors")
+    resultados = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('show_doctors.html',doctores=resultados)
+
+@app.route('/login_patient', methods=['POST', 'GET'])
+def login_patient():
+    try:
+        conn = sqlite3.connect(database_path)
+        cursor = conn.cursor()
+
+        if request.method == 'POST':
+            documento = request.form['documento']
+            contrasena = request.form['contrasena']
+
+            cursor.execute("SELECT * FROM Patients WHERE document=? AND password=?", (documento, contrasena))
+            paciente = cursor.fetchone()
+
+            if paciente:
+                return redirect(url_for('predict_section', paciente_id=paciente[0]))
+
+    except Exception as e:
+        print("Error al conectar a la base de datos:", e)
+
+    finally:
+        if conn:
+            conn.close()
+
+    return render_template('login_patient.html')
+
+
+@app.route('/predict_section/<int:paciente_id>',methods=['POST','GET'])
+def predict_section(paciente_id):
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+    cursor.execute('SELECT name, lastName FROM Patients WHERE id = ?', (paciente_id,))
+    result = cursor.fetchone()
+    conn.close()
+
+    if result:
+        paciente_name = result[0]
+        paciente_lastname = result[1]
+        nombre=paciente_name+" "+paciente_lastname
+
+        return render_template('predict_section.html', paciente_name=nombre,paciente_id=paciente_id)
+    else:
+        abort(404)
+    
+@app.route('/mostrar_imagen/<int:paciente_id>',methods=['POST','GET'])
+def upload_image(paciente_id):
+    if 'file' not in request.files:
+        return render_template('index.html')
+
+    if 'file' not in request.files:
+        return render_template('index.html')
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return render_template('index.html')
+
+    if file and allowed_file(file.filename):
+        uploaded_image = file.read()
+        encoded_image = base64.b64encode(uploaded_image).decode('utf-8')
+
+        image = cv2.imdecode(np.fromstring(uploaded_image, np.uint8), cv2.IMREAD_UNCHANGED)
+        resized_image = cv2.resize(image, (256, 256))        
+        flattened_image = resized_image.reshape(-1)
+        predicted_label = svm_model.predict([flattened_image])
+        predicted_class = disease_folders[int(predicted_label)]
+        if predicted_class == "PSORIASIS":
+            conn = sqlite3.connect(database_path)
+            cursor = conn.cursor()
+            cursor.execute('UPDATE Patients SET disease = ? WHERE id = ?', (predicted_class, paciente_id))
+            conn.commit()
+            conn.close()
+            return render_template('case_psoriasis.html',mensaje=predicted_class,image=encoded_image)
+        if predicted_class == "ROSACEA":
+            conn = sqlite3.connect(database_path)
+            cursor = conn.cursor()
+            cursor.execute('UPDATE Patients SET disease = ? WHERE id = ?', (predicted_class, paciente_id))
+            conn.commit()
+            conn.close()
+            return render_template('case_rosacea.html',mensaje=predicted_class,image=encoded_image)
+        if predicted_class == "SARPULLIDO":
+            conn = sqlite3.connect(database_path)
+            cursor = conn.cursor()
+            cursor.execute('UPDATE Patients SET disease = ? WHERE id = ?', (predicted_class, paciente_id))
+            conn.commit()
+            conn.close()
+            return render_template('case_sarpullido.html',mensaje=predicted_class,image=encoded_image)
+        if predicted_class == "VITILIGIO":
+            conn = sqlite3.connect(database_path)
+            cursor = conn.cursor()
+            cursor.execute('UPDATE Patients SET disease = ? WHERE id = ?', (predicted_class, paciente_id))
+            conn.commit()
+            conn.close()
+            return render_template('case_vitiligio.html',mensaje=predicted_class,image=encoded_image)        
+        return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=500)
---
+++ smv.py
@@ -0,0 +1,49 @@
+import os
+import cv2
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import imgaug.augmenters as iaa
+from sklearn import svm
+import joblib
+
+
+
+seq = iaa.Sequential([
+    iaa.Fliplr(0.5),         
+    iaa.Affine(rotate=(-30, 30)),  
+    iaa.GaussianBlur(sigma=(0, 2.0)),  
+    iaa.AdditiveGaussianNoise(scale=(0, 0.05*255))  
+])
+
+image_data = []
+labels = []
+
+disease_folders = ["PSORIASIS", "ROSACEA", "SARPULLIDO", "VITILIGIO"]
+for i, folder in enumerate(disease_folders):
+    for filename in os.listdir("DATA_SET/" + folder):
+        img = cv2.imread(os.path.join("DATA_SET", folder, filename))
+        img = cv2.resize(img, (256, 256))
+        augmented_images = [] 
+        for _ in range(10):
+            augmented_image = seq.augment_image(img)
+            flattened_image = augmented_image.reshape(-1)
+            augmented_images.append(flattened_image)
+        image_data.extend(augmented_images)
+        labels.extend([i] * len(augmented_images))
+
+image_data = np.array(image_data)
+labels = np.array(labels)
+
+X_train, X_test, y_train, y_test = train_test_split(image_data, labels, test_size=0.2, random_state=42)
+svm_model = svm.SVC(kernel='linear', C=1)
+svm_model.fit(X_train, y_train)
+
+y_pred = svm_model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Precisión del modelo: {accuracy}')
+
+model_filename = 'svm_model.joblib'
+joblib.dump(svm_model, model_filename)
+
+print(f"Modelo SVM guardado en {model_filename}")
